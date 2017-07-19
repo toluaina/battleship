da66567a8949dcba19b5eb9c9c8674d4596213c5
toluaina <toluaina@users.noreply.github.com>
2017-07-19 18:57:17 +0100

    Add files via upload
    Added error.py

---
+++ error.py
@@ -0,0 +1,11 @@
+class GridOutOfBoundError(Exception):
+    pass
+
+class SunkShipError(Exception):
+    pass
+
+class ItemInPositionError(Exception):
+    pass
+
+class MissileDepletedError(Exception):
+    pass
